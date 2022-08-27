e5e12b7eb066f93d53a4bba1dd8dc589eb5ca06b
vikas kumar <108992398+vikaskr775246@users.noreply.github.com>
2022-08-27 06:56:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector('#menu');
+const nav = document.querySelector('#nav');
+function click_fun() {
+    menu.classList.toggle("show");
+    nav.classList.toggle("active");
+}
